a38fe4f1034e629263a33b9affd5b4c4777a5a90
ZealousCynic <ZealousCynic@gmail.com>
2020-04-20 17:35:07 +0200

    Removed unnecessary usings

--- AsymmetricEncryptionExercise.Decryptor/AEReceiverViewModel.cs
+++ AsymmetricEncryptionExercise.Decryptor/AEReceiverViewModel.cs
@@ -1,12 +1,9 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace AsymmetricEncryptionExercise.Decryptor
--- AsymmetricEncryptionExercise/AESenderViewModel.cs
+++ AsymmetricEncryptionExercise/AESenderViewModel.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.IO;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace AsymmetricEncryptionExercise.Encryptor
